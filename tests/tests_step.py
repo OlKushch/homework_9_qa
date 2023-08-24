536d2723f40b4dfd56d64a9b43c49ce1f2be35ef
Olga Belyaeva <olgakushch96@gmail.com>
2023-08-24 18:38:11 +0300

    домашняя работа 9 урок

---
+++ tests/tests_step.py
@@ -0,0 +1,26 @@
+import allure
+from selene.support import by
+from selene.support.conditions import be
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+
+def test_dynamic_steps():
+    with allure.step("Открываем главную страницу"):
+        browser.open('/')
+
+    with allure.step("Ищем репозитория"):
+        s(".header-search-button").click()
+        s("#query-builder-test").send_keys("eroshenkoam/allure-example")
+        s(".ActionListItem-label").submit()
+
+    with allure.step("Переходим по ссылке репозитория"):
+        s(by.link_text("eroshenkoam/allure-example")).click()
+
+    with allure.step("Открываем таб Issues"):
+        s("#issues-tab").click()
+
+    with allure.step("Проверяем наличие Issue с номером 76"):
+        s(by.partial_text("#76")).should(be.visible)
+
+
